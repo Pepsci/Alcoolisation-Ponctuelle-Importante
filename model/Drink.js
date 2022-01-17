e2548551353fa25aa2aacdcc8c4d747919197ddb
Pepsci <jossfilippi@gmail.com>
2022-01-17 11:48:06 +0100

    add model modelDrink

---
+++ model/Drink.js
@@ -0,0 +1,28 @@
+const { model, Schema, mongo } = require("mongoose");
+
+const drinkSchema = new Schema({
+  name: {
+    type: String,
+    enum: [
+      "largeBeer",
+      "smallBeer",
+      "smallShooter",
+      "largeShooter",
+      "bottleBeer",
+      "shortDrink",
+      "longDrink",
+      "wine",
+      "champagne",
+      "strongAlcool",
+    ],
+  },
+  image: String,
+  size: {
+    type: Number,
+    enum: [50, 33, 25, 17, 14, 12, 7, 4, 2],
+  },
+});
+
+const drinkModel = mongoose.model("drink", drinkSchema);
+
+module.exports = drinkModel;
