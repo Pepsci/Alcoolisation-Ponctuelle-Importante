e96ed9e6238bf0c65e75dd8a7d69ac678614beda
Pepsci <jossfilippi@gmail.com>
2022-01-18 22:11:42 +0100

    fix cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
@@ -12,6 +13,11 @@ cloudinary.config({
 // cloudinary : SAAS platform : specialized in images hosting (tools : metadata, image analyzing ...)
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
+  folder: "folder-name", // The name of the folder in cloudinary
+  allowedFormats: ["jpg", "png"],
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // The file on cloudinary would have the same name as the original file name
+  },
 });
 
 const fileUploader = multer({ storage });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const uploader = require('../config/cloudinary')
-const drinkModel = require("../model/Drink")
+const uploader = require("../config/cloudinary");
+const drinkModel = require("../model/Drink");
 
 router.get("/", (req, res, next) => {
   res.render("index", {
@@ -9,51 +9,55 @@ router.get("/", (req, res, next) => {
   });
 });
 
-router.get('/dashboard', (req, res, next) => {
-  res.render('dashboard/drink-manage.hbs')
+router.get("/dashboard", (req, res, next) => {
+  res.render("dashboard/drink-manage.hbs");
 });
 
-router.get('/drink-add', (req, res, next) => {
-  res.render('dashboard/drink-add.hbs')
+router.get("/drink-add", (req, res, next) => {
+  res.render("dashboard/drink-add");
 });
 
-router.post('/drink-add', uploader.single("image"), async (req, res, next) => {
-  const newDrink = {...req.body}
+router.post("/drink-add", uploader.single("image"), async (req, res, next) => {
+  const newDrink = { ...req.body };
+  console.log(uploader);
+  console.log(newDrink);
   if (!req.file) newDrink.image = undefined;
   else newDrink.image = req.file.path;
+
   try {
-  await drinkModel.create(newDrink)
-  res.redirect("/dashboard/drink-manage")
-  }
-  catch(e){
-    res.redirect("/")
+    await drinkModel.create(newDrink);
+    res.redirect("/drink-manage");
+  } catch (err) {
+    next(err);
   }
 });
 
-router.get('/drink-update/:id', async (req, res, next) => {
+router.get("/drink-update/:id", async (req, res, next) => {
   try {
-  const drink = await drinkModel.findById(req.params.id)
-  res.render("dashboard/drink-update.hbs", {drinks})
-  }
-  catch(e){
-    next(e)
+    const drink = await drinkModel.findById(req.params.id);
+    res.render("dashboard/drink-update.hbs", { drink });
+  } catch (e) {
+    next(e);
   }
 });
 
-router.post('/drink-update/:id', uploader.single("image"), async (req,res, next) => {
-  try {
-    const updatedDrink = {...req.body}
-    if (req.file) updatedDrink.image = req.file.path
-await drinkModel.findByIdAndUpdate(req.params.id, updatedDrink)
-res.redirect("dashboard/drink-manage.hbs")
+router.post(
+  "/drink-update/:id",
+  uploader.single("image"),
+  async (req, res, next) => {
+    try {
+      const updatedDrink = { ...req.body };
+      if (req.file) updatedDrink.image = req.file.path;
+      await drinkModel.findByIdAndUpdate(req.params.id, updatedDrink);
+      res.redirect("dashboard/drink-manage.hbs");
+    } catch (e) {
+      next(e);
+    }
   }
-  catch(e){
-    next(e)
-  }
-});
+);
 
-router.get('/drink-manage', (req, res, next) => {
-  res.render("dashboard/drink-manage.hbs")
-})
+router.get("/drink-manage", (req, res, next) => {
+  res.render("dashboard/drink-manage.hbs");
+});
 
 module.exports = router;
