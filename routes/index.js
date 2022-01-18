1c214b66ce98a6092083fb37d4cf35b13b33b1e8
Nicolas Casabianca <nicolas_casabianca@hotmail.fr>
2022-01-18 18:12:35 +0100

    annoying cloudinary

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const uploader = require('../config/cloudinary')
 const drinkModel = require("../model/Drink")
 
@@ -11,42 +10,42 @@ router.get("/", (req, res, next) => {
 });
 
 router.get('/dashboard', (req, res, next) => {
-  res.render('../views/dashboard/drink-manage.hbs')
+  res.render('dashboard/drink-manage.hbs')
 });
 
 router.get('/drink-add', (req, res, next) => {
-  res.render('../views/dashboard/drink-add.hbs')
+  res.render('dashboard/drink-add.hbs')
 });
 
 router.post('/drink-add', uploader.single("image"), async (req, res, next) => {
-  try {
   const newDrink = {...req.body}
-  if (!req.file) drinkModel.image = undefined;
-  else drinkModel.image = req.file.path;
+  if (!req.file) newDrink.image = undefined;
+  else newDrink.image = req.file.path;
+  try {
   await drinkModel.create(newDrink)
-  res.redirect("../views/dashboard/drink-manage.hbs")
+  res.redirect("/dashboard/drink-manage")
   }
   catch(e){
-    next(e)
+    res.redirect("/")
   }
 });
 
-router.get('/drink-update', async (req, res, next) => {
+router.get('/drink-update/:id', async (req, res, next) => {
   try {
   const drink = await drinkModel.findById(req.params.id)
-  res.render("../views/dashboard/drink-update.hbs", {drinks})
+  res.render("dashboard/drink-update.hbs", {drinks})
   }
   catch(e){
     next(e)
   }
 });
 
-router.post('/drink-update', uploader.single("image"), async (req,res, next) => {
+router.post('/drink-update/:id', uploader.single("image"), async (req,res, next) => {
   try {
     const updatedDrink = {...req.body}
     if (req.file) updatedDrink.image = req.file.path
 await drinkModel.findByIdAndUpdate(req.params.id, updatedDrink)
-res.redirect("../views/dashboard/drink-manage.hbs")
+res.redirect("dashboard/drink-manage.hbs")
   }
   catch(e){
     next(e)
@@ -54,7 +53,7 @@ res.redirect("../views/dashboard/drink-manage.hbs")
 });
 
 router.get('/drink-manage', (req, res, next) => {
-  res.render("../views/dashboard/drink-manage.hbs")
+  res.render("dashboard/drink-manage.hbs")
 })
 
 module.exports = router;
