ef314430bf2d37ec0d2bd1ce739917dbb0b3496a
Pepsci <jossfilippi@gmail.com>
2022-01-18 10:04:45 +0100

    correction erreurs

--- config/mongo.js
+++ config/mongo.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost/Alcoolisation-Ponctuelle-Importante", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((x) =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch((err) => console.error("Error connecting to mongo", err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () =>
+  console.log("yay mongodb connected :)")
+);
+
+mongoose.connection.on("error", () => console.log("nay db error sorry :("));
