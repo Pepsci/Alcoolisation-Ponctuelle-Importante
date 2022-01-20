f5784d69bde0ed60175629806ecf640c98c1c55e
Pepsci <jossfilippi@gmail.com>
2022-01-20 17:49:29 +0100

    config mongo

--- config/mongo.js
+++ config/mongo.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://Pepsi:Mag4liftp91#@cluster0.dlchm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((x) =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
